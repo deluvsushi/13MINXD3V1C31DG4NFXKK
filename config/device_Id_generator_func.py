4cfa5e2b479ac79193be1fb95052ce8916a04ed1
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-10-08 19:01:22 +0600

    Update device_Id_generator_func.py

--- config/device_Id_generator_func.py
+++ config/device_Id_generator_func.py
@@ -20,14 +20,3 @@ def auth(email: str = None, phone: str = None, password: str = None):
             data["phoneNumber"] = phone
         request = requests.post("https://aminoapps.com/api/auth", json=data)
         return request.json()
-
-def device_Id_generator():
-	global r
-	r = r + 1
-	try:
-		authorization = auth(email=email, password=password)
-		device_Id = authorization["result"]["url"].split("=")[4]
-		device_Ids = open("deviceids.txt", "a+")
-		device_Ids.write(f"{device_Id}\n")
-	except:
-		return
