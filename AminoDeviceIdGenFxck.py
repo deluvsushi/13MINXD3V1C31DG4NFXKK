cce29b5eb8cfa03cc47bf068393cd0c7c9fb119b
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-10-08 18:19:50 +0600

    Create AminoDeviceIdGenFxck.py

---
+++ AminoDeviceIdGenFxck.py
@@ -0,0 +1,29 @@
+import pyfiglet
+from config import device_Id_generator_func as AminoLab
+from threading import Thread
+from colored import fore, back, style, attr
+attr(0)
+print(fore.DEEP_SKY_BLUE_1 + style.BOLD)
+print("""Script by Lil Zevi
+Github : https://github.com/LilZevi""")
+print(pyfiglet.figlet_format("aminodeviceidgenfxck", font="smslant", width=58))
+r = 0    
+email = input("Email >> ")
+password = input("Password >> ")
+
+# device_Id generator
+def device_Id_generator():
+	global r
+	r = r + 1
+	try:
+		authorization = AminoLab.auth(email=email, password=password)
+		device_Id = authorization["result"]["url"].split("=")[4]
+		print(f"device_Id: {device_Id}")
+		device_Ids = open("deviceids.txt", "a+")
+		device_Ids.write(f"{device_Id}\n")
+	except:
+		return
+
+for r in range(25):
+	while True:
+		Thread(target=device_Id_generator).start()
